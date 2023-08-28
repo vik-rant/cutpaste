2a72c3745ee85a2b75fad97225bd47a046fe04d3
Vikrant Sharma <7.sharma.vikrant@gmail.com>
2023-08-28 20:04:07 +0530

    add console script; basic README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cutpaste
 cut and paste with multi-char delimiters
+
+### Installation
+```bash
+pip install cutpaste
+```  
+
+### Usage
+```bash
+pycut -d ' ' -f 1 /path/to/some_space_delimited_file.txt
+```  
+
+```bash
+pypaste -d '\t' column_a.txt column_b.txt
+```  
---
+++ cutpaste/cut.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+from cutpaste.version import version
+
+
+def main():
+    p = argparse.ArgumentParser('pycut')
+    p.add_argument('-V', '--version', action='version', version='%(prog)s ' + version)
+    args = p.parse_args()
+
+
+if __name__ == '__main__':
+    main()
